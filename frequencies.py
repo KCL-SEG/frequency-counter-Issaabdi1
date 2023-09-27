c12c0e317d2944d910cf6899b58d4eb886c18aaa
Issa Abdi <issa.abdi@kcl.ac.uk>
2023-09-27 08:42:01 +0100

    frequency assignment complete

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,13 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    string_items = map(str, items)
+    for i in string_items:
+        if i in frequencies:
+            frequencies[i] = frequencies[i] + 1
+        else:
+            frequencies[i] = 1
     return frequencies
+
+frequencies(['a', 'a', 'b', 'b', 'b', 'c'])
+frequencies([100, 'Hello', '100', '100', 100])
